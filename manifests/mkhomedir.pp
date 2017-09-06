06180cd0899f19d90ac7502bb2764ca90f00df3c
Sascha Kettler <sascha.kettler@gronau-it-cloud-computing.de>
2017-09-06 10:10:13 +0200

    Added support for Debian/Ubuntu

--- manifests/mkhomedir.pp
+++ manifests/mkhomedir.pp
@@ -13,6 +13,21 @@ class oddjob::mkhomedir (
 
   include 'oddjob'
 
+  if $facts['os']['name'] in ['RedHat','CentOS'] {
+    $_libdir = '/usr/libexec'
+  }
+  elsif $facts['os']['name'] in ['Debian','Ubuntu'] {
+    if $facts['architecture'] == 'amd64' {
+      $_libdir = '/usr/lib/x86_64-linux-gnu'
+    }
+    else {
+      fail("Only 64-bit ${facts['os']['name']} is supported by '${module_name}'")
+    }
+  }
+  else {
+    fail("OS '${facts['os']['name']}' not supported by '${module_name}'")
+  }
+
   package { 'oddjob-mkhomedir': ensure => 'latest' }
 
   file { '/etc/oddjobd.conf.d/oddjobd-mkhomedir.conf':
